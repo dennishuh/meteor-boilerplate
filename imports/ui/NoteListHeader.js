b1daa575b3ae56f12f3aaba27a081c7e7cb05e79
Dennis Huh <dhuh@gogoair.com>
2017-08-10 00:55:45 -0500

    Notes update

--- imports/ui/NoteListHeader.js
+++ imports/ui/NoteListHeader.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data';
+import { Session } from 'meteor/session';
 
 const NoteListHeader = (props) => {
   return (
     <div>
       <button onClick={() => {
-        props.meteorCall('notes.insert')
+        props.meteorCall('notes.insert', (err, res) => {
+          if (res) {
+            props.Session.set('selectedNoteId', res);
+          }
+        });
       }}>Create Note</button>
     </div>
   )
@@ -18,6 +23,7 @@ NoteListHeader.propType = {
 
 export default createContainer(() => {
   return {
-    meteorCall: Meteor.call
+    meteorCall: Meteor.call,
+    Session
   };
 }, NoteListHeader)
