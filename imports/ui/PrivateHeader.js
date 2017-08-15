51d1b0163da609bf287889f54620de429852e016
Dennis Huh <dhuh@gogoair.com>
2017-08-14 22:15:41 -0500

    Set up mobile navigation

--- imports/ui/PrivateHeader.js
+++ imports/ui/PrivateHeader.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Accounts } from 'meteor/accounts-base';
+import { createContainer } from 'meteor/react-meteor-data';
+import { Session } from 'meteor/session';
 
-const PrivateHeader = (props) => {
+export const PrivateHeader = (props) => {
+  const navImageSrc = props.isNavOpen ? '/images/x.svg' : '/images/bars.svg';
   return (
     <div className="header">
       <div className="header__content">
+        <img className="header__nav-toggle" src={navImageSrc} onClick={props.handleNavToggle}/>
         <h1 className="header__title">{props.title}</h1>
         <button className="button button--link-text" onClick={() => Accounts.logout()}>Logout</button>
       </div>
@@ -16,4 +20,10 @@ PrivateHeader.propTypes = {
   title: React.PropTypes.string.isRequired
 };
 
-export default PrivateHeader;
+export default createContainer(() => {
+  return {
+    handleLogout: () => Accounts.logout(),
+    handleNavToggle: () => Session.set('isNavOpen', !Session.get('isNavOpen')),
+    isNavOpen: Session.get('isNavOpen')
+  }
+}, PrivateHeader);
